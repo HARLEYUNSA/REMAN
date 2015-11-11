7d64413eb7d495bd723789fb48eddf71f6bf4ae7
Gonzalo <glunaluza@gmail.com>
2015-11-11 18:19:33 -0500

    Modificación del Libro de Actores para exportar a PDF

--- REMAN/src/org/harley/reman/clases/LibroActores.java
+++ REMAN/src/org/harley/reman/clases/LibroActores.java
@@ -1,130 +1,55 @@
-
 package org.harley.reman.clases;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "", propOrder = {
+@XmlRootElement(name = "libroActores")
+@XmlType(propOrder = {
     "titulo",
     "intro",
     "actores"
 })
-@XmlRootElement(name = "libroActores")
+
 public class LibroActores {
+    String titulo;
+    String intro;
+    List<Actor> actores;
 
-    @XmlElement(required = true)
-    protected String titulo;
-    @XmlElement(required = true)
-    protected String intro;
-    @XmlElement(required = true)
-    protected LibroActores.Actores actores;
-
-    public String getTitulo() {
+     public String getTitulo() {
         return titulo;
     }
-
-    public void setTitulo(String value) {
-        this.titulo = value;
+    
+    @XmlElement
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     public String getIntro() {
         return intro;
     }
-
-    public void setIntro(String value) {
-        this.intro = value;
+    
+    @XmlElement
+    public void setIntro(String intro) {
+        this.intro = intro;
     }
 
-    public LibroActores.Actores getActores() {
+    public List<Actor> getActores() {
         return actores;
     }
-
-    public void setActores(LibroActores.Actores value) {
-        this.actores = value;
+    @XmlElementWrapper(name = "actores")
+    @XmlElement(name = "actor")
+    public void setActores(List<Actor> actores) {
+        this.actores = actores;
     }
-
-
-    @XmlAccessorType(XmlAccessType.FIELD)
-    @XmlType(name = "", propOrder = {
-        "actor",
-        "org",
-        "cargo",
-        "tipo",
-        "correo",
-        "comentarios"
-    })
-    public static class Actores {
-
-        @XmlElement(required = true)
-        protected String actor;
-        @XmlElement(required = true)
-        protected String org;
-        @XmlElement(required = true)
-        protected String cargo;
-        @XmlElement(required = true)
-        protected String tipo;
-        @XmlElement(required = true)
-        protected String correo;
-        @XmlElement(required = true)
-        protected String comentarios;
-
-        public String getActor() {
-            return actor;
+    
+    public void addActor(Actor act){
+        if( this.actores == null ){
+            this.actores = new ArrayList<>();
         }
-
-        public void setActor(String value) {
-            this.actor = value;
-        }
-
-        public String getOrg() {
-            return org;
-        }
-
-        public void setOrg(String value) {
-            this.org = value;
-        }
-
-        public String getCargo() {
-            return cargo;
-        }
-
-        public void setCargo(String value) {
-            this.cargo = value;
-        }
-
-        public String getTipo() {
-            return tipo;
-        }
-
-        
-        public void setTipo(String value) {
-            this.tipo = value;
-        }
-
-        
-        public String getCorreo() {
-            return correo;
-        }
-
-        
-        public void setCorreo(String value) {
-            this.correo = value;
-        }
-
-        public String getComentarios() {
-            return comentarios;
-        }
-
-
-        public void setComentarios(String value) {
-            this.comentarios = value;
-        }
-
+            this.actores.add(act);
     }
-
 }
