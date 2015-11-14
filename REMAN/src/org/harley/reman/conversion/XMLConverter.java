7e6bd753a4a3545d9eb07aa782c28eace4edd7cd
Gonzalo <glunaluza@gmail.com>
2015-11-13 21:54:21 -0500

    Documentación de XMLReader y XMLConverter
    Se documentó utilizando Javadoc las clases XMLReader y XMLConverter

--- REMAN/src/org/harley/reman/conversion/XMLConverter.java
+++ REMAN/src/org/harley/reman/conversion/XMLConverter.java
@@ -30,27 +30,30 @@ import org.apache.fop.apps.MimeConstants;
 import org.apache.fop.apps.PageSequenceResults;
 
 /**
- * This class demonstrates the convert of a XML file to PDF using FOP.
+ * Clase que convierte archivos xml a pdf usando fop
+ * @author Gonzalo
  */
-
 public class XMLConverter {
 
-    // Configure FopFactory
     private final FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
     File databaseDir;
     File baseDir;
     File outDir;
-        
+    
+    /**
+     * Constructor de la clase <code>XMLConverter</code>
+     */
     public  XMLConverter (){
         databaseDir = new File("src/org/harley/reman/database");
         outDir = new File("src/org/harley/reman/informes");
         baseDir = new File("src/org/harley/reman/conversion/plantillas");
     }
+    
     /**
-     * Converts an XSL and XML file to a FO file 
-     * @param xsl the Stylesheet file
-     * @param xml the XML file
-     * @param fo the target FO file
+     * Convierte un archivo XSL y XML file to a FO file 
+     * @param xsl Hoja de estilos
+     * @param xml Archivo XML
+     * @param fo Archivo fo destino
      */
     public void convertXML2FO(File xsl, File xml, File fo){
 
@@ -74,9 +77,9 @@ public class XMLConverter {
     }
     
      /**
-     * Converts an FO file to a PDF file using FOP
-     * @param fo the FO file
-     * @param pdf the target PDF file
+     * Convierte un archivo FO a PDF usando FOP
+     * @param fo Archivo FO origen
+     * @param pdf Archivo PDF destino
      */
     public void convertFO2PDF(File fo, File pdf){
 
@@ -104,7 +107,7 @@ public class XMLConverter {
             transformer.transform(src, res);
 
             // Result processing
-            FormattingResults foResults = fop.getResults();
+            /*FormattingResults foResults = fop.getResults();
             List pageSequences = foResults.getPageSequences();
             for (Iterator it = pageSequences.iterator(); it.hasNext();) {
                 PageSequenceResults pageSequenceResults = (PageSequenceResults)it.next();
@@ -114,7 +117,7 @@ public class XMLConverter {
                         + " generated " + pageSequenceResults.getPageCount() + " pages.");
             }
             System.out.println("Generated " + foResults.getPageCount() + " pages in total.");
-
+*/
         } catch (FileNotFoundException | FOPException | TransformerException e) {
             e.printStackTrace(System.err);
             System.exit(-1);
@@ -127,12 +130,17 @@ public class XMLConverter {
         }
     }
 
-    public void convert(String archivo, String nombre){
+    /**
+     * Convierte el archivo XML en PDF
+     * @param plantilla Archivo XLS para dar formato
+     * @param archivoXML Archivo XML de origen
+     */
+    public void convert(String plantilla, String archivoXML){
         //Setup input and output files
-        File xmlFile = new File(databaseDir, nombre +".xml");
-        File xslFile = new File(baseDir, archivo +".xsl");
-        File foFile = new File(databaseDir, nombre +".fo");
-        File pdfFile = new File(outDir, nombre +".pdf");
+        File xmlFile = new File(databaseDir, archivoXML +".xml");
+        File xslFile = new File(baseDir, plantilla +".xsl");
+        File foFile = new File(databaseDir, archivoXML +".fo");
+        File pdfFile = new File(outDir, archivoXML +".pdf");
 
         //Status
         System.out.println("Input: XML (" + xmlFile + ")");
@@ -146,6 +154,13 @@ public class XMLConverter {
         System.out.println("Success!");
     }
 
+    /**
+     * Convierte un archivo XML a PDF
+     * @param xml Archivo XML origen
+     * @param xsl Archivo XSL origen
+     * @param fo Archivo FO intermedio
+     * @param pdf Archivo PDF destino
+     */
     public void convertXML2PDF(File xml, File xsl, File fo, File pdf){
         
         //Convert from XML and XLS to FOP
@@ -155,14 +170,25 @@ public class XMLConverter {
         convertFO2PDF(fo, pdf);
         //Delete temporal directory
         clean(fo);
-        
-        try {
-            Desktop.getDesktop().open(pdf);
+        abrirPDF(pdf);
+    }
+    
+    /**
+     * Abre el archivo PDF 
+     * @param archivoPDF Archivo PDF que se desea abrir
+     */
+    public void abrirPDF(File archivoPDF){
+         try {
+            Desktop.getDesktop().open(archivoPDF);
         } catch (IOException ex) {
             Logger.getLogger(XMLConverter.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
+    /**
+     * Elimina el archivo FO intermedio
+     * @param fo Archivo FO creado
+     */
     public void clean(File fo){
         fo.delete();
     }
--- REMAN/src/org/harley/reman/conversion/XMLReader.java
+++ REMAN/src/org/harley/reman/conversion/XMLReader.java
@@ -6,40 +6,59 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+/**
+ * Clase que permite leer y escribir documentos xml.
+ * @author Gonzalo
+ * @param <T> Tipo de objeto para instanciar la clase
+ */
 public class XMLReader<T> {
     final Class<T> typeParameterClass;
     File database;
     
+    /**
+     * Constructor de la clase <code>XMLReader</code>
+     * @param typeParameterClass 
+     */
     public XMLReader(Class<T> typeParameterClass) {
         this.database = new File("src/org/harley/reman/database");
         this.typeParameterClass = typeParameterClass;
         database.mkdirs();
     }
 
-    public void writeXML(String archivo, T result){
+    /**
+     * Escribe los atributos de un objeto en un archivo xml
+     * @param nombreArchivo Nombre del archivo xml destino
+     * @param objeto        Objeto con atributos a escribir
+     */
+    public void writeXML(String nombreArchivo, T objeto){
         try {
-            File xmlFile = new File(database, archivo + ".xml");
+            File xmlFile = new File(database, nombreArchivo + ".xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(typeParameterClass);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
-            // output pretty printed
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            jaxbMarshaller.marshal(result, xmlFile);
-            jaxbMarshaller.marshal(result, System.out);
+            jaxbMarshaller.marshal(objeto, xmlFile);
+            
+            //Imprime en pantalla el archivo xml
+            //jaxbMarshaller.marshal(objeto, System.out);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
     }
     
-    public T openXML(String archivo){
+    /**
+     * Abre un archivo xml y lo guarda en un objeto.
+     * @param nombreArchivo Nombre del archivo xml origen.
+     * @return Objeto que contiene la información del archivo xml.
+     */
+    public T openXML(String nombreArchivo){
         try {
-            File xmlFile = new File(database, archivo + ".xml");
+            File xmlFile = new File(database, nombreArchivo + ".xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(typeParameterClass);
-
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            T custom = (T) jaxbUnmarshaller.unmarshal(xmlFile);
-            return custom;
+            
+            T object = (T) jaxbUnmarshaller.unmarshal(xmlFile);
+            return object;
         } catch (JAXBException e) {
             e.printStackTrace();
         }
